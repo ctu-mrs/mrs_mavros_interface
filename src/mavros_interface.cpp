65185e43647cc85ac6d5e4742793f75d18ccaeac
klaxalk <Tomas Baca>
2018-07-08 19:23:00 +0200

    removed penn_controller; added mrs_mavros_interface

---
+++ src/mavros_interface.cpp
@@ -0,0 +1,87 @@
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <mutex>
+#include <tf/LinearMath/Transform.h>
+
+class MavrosInterface {
+
+public:
+  MavrosInterface();
+
+private:
+  ros::NodeHandle nh_;
+  ros::Subscriber subscriber_odometry;
+  ros::Publisher  publisher_odometry;
+
+private:
+  void callbackOdometry(const nav_msgs::OdometryConstPtr &msg);
+};
+
+// constructor
+MavrosInterface::MavrosInterface() {
+
+  nh_ = ros::NodeHandle("~");
+
+  ros::Time::waitForValid();
+
+  subscriber_odometry = nh_.subscribe("odometry_in", 1, &MavrosInterface::callbackOdometry, this, ros::TransportHints().tcpNoDelay());
+
+  publisher_odometry = nh_.advertise<nav_msgs::Odometry>("odometry_out", 1);
+}
+
+void MavrosInterface::callbackOdometry(const nav_msgs::OdometryConstPtr &msg) {
+
+  nav_msgs::Odometry updated_odometry = *msg;
+
+  // --------------------------------------------------------------
+  // |        extract the vectors from the original message       |
+  // --------------------------------------------------------------
+
+  tf::Vector3 velocity(msg->twist.twist.linear.x, msg->twist.twist.linear.y, msg->twist.twist.linear.z);
+  tf::Vector3 angular(msg->twist.twist.angular.x, msg->twist.twist.angular.y, msg->twist.twist.angular.z);
+
+  // --------------------------------------------------------------
+  // |           prepare the quaternion for the rotation          |
+  // --------------------------------------------------------------
+
+  tf::Quaternion rotation(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z,
+                          msg->pose.pose.orientation.w);
+
+  // --------------------------------------------------------------
+  // |                     rotate the vectors                     |
+  // --------------------------------------------------------------
+
+  tf::Vector3 rotated_velocity = tf::quatRotate(rotation, velocity);
+  tf::Vector3 rotated_angular = tf::quatRotate(rotation, angular);
+
+  // --------------------------------------------------------------
+  // |        update the odometry message with the new data       |
+  // --------------------------------------------------------------
+
+  updated_odometry.twist.twist.linear.x = rotated_velocity[0];
+  updated_odometry.twist.twist.linear.y = rotated_velocity[1];
+  updated_odometry.twist.twist.linear.z = rotated_velocity[2];
+
+  updated_odometry.twist.twist.angular.x = rotated_angular[0];
+  updated_odometry.twist.twist.angular.y = rotated_angular[1];
+  updated_odometry.twist.twist.angular.z = rotated_angular[2];
+
+  updated_odometry.child_frame_id = "local_origin";
+
+  // --------------------------------------------------------------
+  // |                  publish the new odometry                  |
+  // --------------------------------------------------------------
+
+  publisher_odometry.publish(updated_odometry);
+}
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "mavros_interface");
+
+  MavrosInterface mavros_interface;
+
+  ros::spin();
+
+  return 0;
+};
